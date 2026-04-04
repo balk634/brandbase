fcd928bd2c675a0880127da69345a3d3487dbd0b
balk634 <balk634@gmail.com>
2026-04-05 01:23:17 +0530

    fix: resolve image failures on Netlify deployment

--- next.config.js
+++ next.config.js
@@ -29,11 +29,6 @@ const nextConfig = {
     minimumCacheTTL: 31536000,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    localPatterns: [
-      {
-        pathname: "/**",
-      },
-    ],
     remotePatterns: [
       {
         protocol: "https",
